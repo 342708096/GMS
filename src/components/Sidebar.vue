38c6de1be2b8077c51aa202f2f86dda846bb8e3e
zhuzheng <zhengzhu@sohu-inc.com>
2017-08-20 00:36:50 +0800

    menu 重构

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,17 +3,27 @@
     <nav class="sidebar-nav">
       <ul class="nav">
         <li class="nav-item">
-          <router-link :to="'/dashboard'" class="nav-link"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-info">NEW</span></router-link>
+          <router-link :to="'/dashboard'" class="nav-link">
+            <i class="icon-speedometer"></i> Dashboard <span class="badge badge-info">NEW</span>
+          </router-link>
         </li>
         <li class="nav-title text-center">
           <span>UI Elements</span>
         </li>
         <li class="nav-item" :class="{'nav-dropdown': menu.children}" v-for="menu in menus" :key="menu.path" v-once>
-          <div class="nav-link" :class="{'nav-dropdown-toggle': menu.children}" @click="handleClick($event, menu)"><i :class="menu.meta.icon"></i> {{menu.meta.menu}}</div>
+          <div v-if="menu.children" class="nav-link nav-dropdown-toggle"
+               @click="$event.currentTarget.parentElement.classList.toggle('open')">
+            <i :class="menu.meta.icon"></i> {{menu.meta.menu}}
+          </div>
+          <router-link tag="div" v-else class="nav-link" :to="{path: '/' + menu.path }" exact>
+            <i :class="menu.meta.icon"></i> {{menu.meta.menu}}
+          </router-link>
           <ul class="nav-dropdown-items" @click.stop>
-            <router-link tag="li" class="nav-item" v-for="menul2 in menu.children" :key="menul2.path" :to="{path: '/' + menu.path +'/'+ menul2.path}" exact>
-              <div class="nav-link" ><i :class="menul2.meta.icon"></i> {{menul2.meta.menu}}</div>
-            </router-link>
+            <li class="nav-item" v-for="menul2 in menu.children" :key="menul2.path">
+              <router-link tag="div" class="nav-link" :to="{path: '/' + menu.path +'/'+ menul2.path}" exact>
+                <i :class="menul2.meta.icon"></i> {{menul2.meta.menu}}
+              </router-link>
+            </li>
           </ul>
         </li>
         <li class="divider"></li>
@@ -28,15 +38,6 @@ export default {
     menus: {
       type: Array,
       default: []
-    }
-  },
-  methods: {
-    handleClick (e, menu) {
-      if (menu.children) {
-        e.target.parentElement.classList.toggle('open')
-      } else {
-        this.$router.replace('/' + menu.path)
-      }
     }
   }
 }
